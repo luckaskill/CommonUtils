da297ed18adcbb00fa79b0245ad50a80468d1574
LAS <dkavialkou@elinext.com>
2021-01-28 06:04:56 +0300

    Collection From Iterable

--- src/main/java/com/las/common/utils/CollectionUtil.java
+++ src/main/java/com/las/common/utils/CollectionUtil.java
@@ -1,9 +1,23 @@
 package com.las.common.utils;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.function.Supplier;
 
 public class CollectionUtil {
     public static boolean isEmpty(Collection<?> collection) {
         return collection == null || collection.isEmpty();
+    }
+
+    public static <T, C extends Collection<T>> Collection<T> fromIterable(Iterable<T> iterable, Supplier<C> collectionCreator) {
+        C collection = collectionCreator.get();
+        for (T t : iterable) {
+            collection.add(t);
+        }
+        return collection;
+    }
+
+    public static <T, C extends Collection<T>> Collection<T> fromIterable(Iterable<T> iterable) {
+        return fromIterable(iterable, ArrayList::new);
     }
 }
